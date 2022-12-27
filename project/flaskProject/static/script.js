317f3fc2482ab2b4f9b703bdc93239e94255f6bb
Faria-34 <118168617+Faria-34@users.noreply.github.com>
2022-12-28 01:41:20 +0800

    Update script.js

--- project/flaskProject/static/script.js
+++ project/flaskProject/static/script.js
@@ -1,25 +1,10 @@
 window.onload = function () {
-    // var btn_add = document.getElementById("btn_add");
-    // var chip = document.getElementById("chip");
-    // var Chip_order = document.getElementById("Chip_order");
+    
     var address = document.getElementById('address')
     var companys = document.querySelectorAll('select[name=company]')
 
 
-    // const consumer_inf = document.getElementById('form_consumer');
-    // consumer_inf.addEventListener('confirm', submit_consumer);
-
-    // function submit_consumer(e) {
-    //     console.log(e)
-    //     e.preventDefault()
-    //     getCompany()
-    //     return false
-    // }
-
-    // //如何得到公司名称？
-    // function getCompany() {
-    //     console.log('get it')
-    // }
+    
 
     //选取地址后给公司列表添加option
     address.addEventListener('change', appendCompany)
@@ -53,20 +38,15 @@ window.onload = function () {
             
           }
     }
+    for (let i = 0; i < companys.length; i++) {
+        value_list = ['A111', 'A666']
+        for (var j = 0; j < value_list.length; j++) {
+            var options = document.createElement('option')
+            options.innerHTML = value_list[j]
+            companys[i].appendChild(options);
+        }
+    }
 
-    // btn_add.addEventListener('click', function () {
-    //     Chip_order.innerHTML=Chip_order.innerHTML + chip.innerHTML
-    //     console.log(1)
-    //     address.setAttribute('disabled', 'disabled')//添加芯片后禁止修改地址
-
-    // })
-
-
-
-    // //submit执行的函数
-    // function fnClose() {
-    //     console.log('success')
-    // }
 
     
     
